1734cf15d1aa438e65fd051f47623625b49c84af
jay-dhanwant <jay.hdfs@gmail.com>
2025-07-16 01:40:33 +0530

    Add report_video_v3 functionality to gRPC service
    - Implemented the `report_video_v3` method in the `MLFeedStub` and `MLFeedServicer` classes to handle video reporting.
    - Updated the gRPC service to include the new `report_video_v3` endpoint.
    - Added corresponding request and response message definitions in the `video_recommendation.proto` file for the new reporting functionality.
    - Enhanced the existing structure to accommodate the new video reporting logic.

--- python_src/recommendation_service/video_recommendation_pb2_grpc.py
+++ python_src/recommendation_service/video_recommendation_pb2_grpc.py
@@ -109,6 +109,11 @@ class MLFeedStub(object):
                 request_serializer=video__recommendation__pb2.MLFeedRequestV3.SerializeToString,
                 response_deserializer=video__recommendation__pb2.MLFeedResponseV3.FromString,
                 _registered_method=True)
+        self.report_video_v3 = channel.unary_unary(
+                '/ml_feed_py.MLFeed/report_video_v3',
+                request_serializer=video__recommendation__pb2.VideoReportRequestV3.SerializeToString,
+                response_deserializer=video__recommendation__pb2.VideoReportResponseV3.FromString,
+                _registered_method=True)
 
 
 class MLFeedServicer(object):
@@ -204,6 +209,12 @@ class MLFeedServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def report_video_v3(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_MLFeedServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -281,6 +292,11 @@ def add_MLFeedServicer_to_server(servicer, server):
                     servicer.get_ml_feed_combined_v3,
                     request_deserializer=video__recommendation__pb2.MLFeedRequestV3.FromString,
                     response_serializer=video__recommendation__pb2.MLFeedResponseV3.SerializeToString,
+            ),
+            'report_video_v3': grpc.unary_unary_rpc_method_handler(
+                    servicer.report_video_v3,
+                    request_deserializer=video__recommendation__pb2.VideoReportRequestV3.FromString,
+                    response_serializer=video__recommendation__pb2.VideoReportResponseV3.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -688,6 +704,33 @@ class MLFeed(object):
             '/ml_feed_py.MLFeed/get_ml_feed_combined_v3',
             video__recommendation__pb2.MLFeedRequestV3.SerializeToString,
             video__recommendation__pb2.MLFeedResponseV3.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def report_video_v3(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/ml_feed_py.MLFeed/report_video_v3',
+            video__recommendation__pb2.VideoReportRequestV3.SerializeToString,
+            video__recommendation__pb2.VideoReportResponseV3.FromString,
             options,
             channel_credentials,
             insecure,
